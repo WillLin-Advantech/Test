48395e2fa1e034df0ae0245a3080e64416b75f6e
WillLin-Advantech <Will1.Lin@advantech.com>
2025-07-15 09:26:49 +0800

    Add hello world api

---
+++ src/HelloWorld.HttpApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers;
+
+[Route("api/hello")]
+public class TestController : AbpController
+{
+    [HttpGet]
+	public string Hello(){
+		
+		return "hello";
+	}
+}
